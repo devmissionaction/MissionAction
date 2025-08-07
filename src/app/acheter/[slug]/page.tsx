83248d4fd7483ed39563ae32036d32f955791fd8
devmissionaction <dev.missionaction@gmail.com>
2025-08-07 12:23:10 +0200

    Manage props error

--- src/app/acheter/[slug]/page.tsx
+++ src/app/acheter/[slug]/page.tsx
@@ -1,9 +1,12 @@
 import AcheterNumeroClient from './AcheterNumeroClient'
 
 type Props = {
-  params: { slug: string }
+  params: {
+    slug: string
+  }
 }
 
-export default function AcheterNumeroPage({ params }: any) {
-  return <AcheterNumeroClient slug={params.slug} />
+export default function AcheterNumeroPage({ params }: Props) {
+    const { slug } = params
+    return <AcheterNumeroClient slug={params.slug} />
 }
