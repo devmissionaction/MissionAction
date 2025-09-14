237bb673775536613aace04a88f181df8cd7a036
devmissionaction <dev.missionaction@gmail.com>
2025-09-15 01:01:14 +0200

    Correctif : redirection après paiement

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -33,8 +33,8 @@ export async function POST(req: Request) {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/merci?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/annule`,
+      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
     })
 
     return NextResponse.json({ url: session.url })
